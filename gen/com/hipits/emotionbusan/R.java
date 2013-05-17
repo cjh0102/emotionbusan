281d5d6741153c6e8627a7476068633454cb2aa3
junhwan <oprt12@gmail.com>
2013-05-17 12:33:31 +0900

    코멘트 추가 코딩 매니페스트 수정

--- gen/com/hipits/emotionbusan/R.java
+++ gen/com/hipits/emotionbusan/R.java
@@ -24,17 +24,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000a;
+        public static final int action_settings=0x7f08000b;
         public static final int commentListView=0x7f080008;
         public static final int contentTextView=0x7f080006;
         public static final int idEditText=0x7f080001;
-        public static final int imageView1=0x7f080009;
+        public static final int imageView1=0x7f08000a;
         public static final int loginButton=0x7f080003;
         public static final int passwordEditText=0x7f080002;
         public static final int talksroomListView=0x7f080000;
         public static final int timeTextView=0x7f080007;
         public static final int titleTextView=0x7f080005;
         public static final int writeButton=0x7f080004;
+        public static final int writeComment=0x7f080009;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/hipits/emotionbusan/activity/TalksRoomDetailActivity.java
+++ src/com/hipits/emotionbusan/activity/TalksRoomDetailActivity.java
@@ -1,5 +1,8 @@
 package com.hipits.emotionbusan.activity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,52 +12,94 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.hipits.emotionbusan.R;
+import com.hipits.emotionbusan.adapter.CommentListAdapter;
 import com.hipits.emotionbusan.baasio.EtcUtils;
+import com.kth.baasio.callback.BaasioCallback;
 import com.kth.baasio.entity.entity.BaasioEntity;
+import com.kth.baasio.exception.BaasioException;
 import com.kth.baasio.utils.JsonUtils;
 import com.kth.baasio.utils.ObjectUtils;
 
 public class TalksRoomDetailActivity extends Activity {
 
-	private BaasioEntity entity;
+	private BaasioEntity postEntity;
+	private List<BaasioEntity> comments;
+	private CommentListAdapter adapter;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_talksroomdetail);
 
-		Intent intent = getIntent();
+		comments = new ArrayList<BaasioEntity>();
+		adapter = new CommentListAdapter(this, comments);
 
+		Intent intent = getIntent();
 		String post = intent.getStringExtra("post");
 
 		if (!ObjectUtils.isEmpty(post)) {
-			entity = JsonUtils.parse(post, BaasioEntity.class);
+			postEntity = JsonUtils.parse(post, BaasioEntity.class);
 		}
 
 		TextView titleTextView = (TextView) findViewById(R.id.titleTextView);
 		TextView contentTextView = (TextView) findViewById(R.id.contentTextView);
 		TextView timeTextView = (TextView) findViewById(R.id.timeTextView);
 
-		setStringToView(entity, titleTextView, "title");
-		setStringToView(entity, contentTextView, "body");
+		setStringToView(postEntity, titleTextView, "title");
+		setStringToView(postEntity, contentTextView, "body");
 
-		if (entity.getCreated() != null) {
-			String createdTime = EtcUtils.getDateString(entity.getCreated());
+		if (postEntity.getCreated() != null) {
+			String createdTime = EtcUtils
+					.getDateString(postEntity.getCreated());
 			timeTextView.setText(createdTime);
 		}
-		
+
 		ListView commentListView = (ListView) findViewById(R.id.commentListView);
-		
-		findViewById(R.id.writeButton).setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View arg0) {
-				
-			}
-		});
+
+		findViewById(R.id.writeComment).setOnClickListener(
+				new OnClickListener() {
+					@Override
+					public void onClick(View arg0) {
+						String body = "comment";
+						writeComment(body);
+					}
+				});
 	}
 
 	private void setStringToView(BaasioEntity entity, TextView view,
 			String value) {
 		view.setText(EtcUtils.getStringFromEntity(entity, value));
+	}
+
+	public void writeComment(String comment) {
+		BaasioEntity commentEntity = new BaasioEntity("comment");
+		commentEntity.setProperty("body", comment);
+
+		commentEntity.saveInBackground(new BaasioCallback<BaasioEntity>() {
+			@Override
+			public void onException(BaasioException arg0) {
+
+			}
+
+			@Override
+			public void onResponse(BaasioEntity response) {
+				postEntity.connectInBackground("write_comment", response,
+						new BaasioCallback<BaasioEntity>() {
+
+					@Override
+					public void onException(BaasioException arg0) {
+
+					}
+
+					@Override
+					public void onResponse(BaasioEntity response) {
+						if (!ObjectUtils.isEmpty(response)) {
+							comments.add(0, response);
+							adapter.notifyDataSetChanged();
+						}
+					}
+				});
+			}
+		});
 	}
 }
